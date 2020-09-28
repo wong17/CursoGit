02acd284a9c753df39f3fd0d73dba3405711d665
wong17 <josuewong797@protonmail.com>
2020-09-28 09:05:20 -0600

    Se agrego un segundo bucle for

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		for(int i=0; i<10; i++) {
 			System.out.println(i);
+			for(int i=0; i<10; i++) {
+				System.out.println(i);
+			}
 		}
 	}
 }
