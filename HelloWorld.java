e8b8741c6b0034302cca4bee0a8ec48eecd07de0
wong17 <josuewong797@protonmail.com>
2020-09-28 09:03:26 -0600

    Bucle for agregado

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
-public class HolaMundo {
+public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("Hola mundo");
+		System.out.println("Hello World");
 		for(int i=0; i<10; i++) {
 			System.out.println(i);
 		}
